7df16ee108bead65a4c389cdcdcef8ef79f48921
helena-cruse <helena.m.cruse@gmail.com>
2025-10-24 13:41:54 +0200

    Completed responsive index (authentication) page with dropdown forms

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./feed/**/*.html", "./profile/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
